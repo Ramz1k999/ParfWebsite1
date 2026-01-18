86f13b68c41a38881898558b660be631054af490
Ramz1k999 <jaloladdin_yaxshimurodov@epam.com>
2026-01-18 08:08:25 -0500

    Update currency.py

--- app/schemas/currency.py
+++ app/schemas/currency.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from datetime import datetime
 
 
@@ -15,5 +15,4 @@ class CurrencyRateResponse(BaseModel):
     created_at: datetime
     updated_at: datetime
 
-    class Config:
-        orm_mode = True
+    model_config = ConfigDict(from_attributes=True)
