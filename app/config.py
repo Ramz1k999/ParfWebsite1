3970899033d0d1c2148db281881064a4ef320af8
Ramz1k999 <jaloladdin_yaxshimurodov@epam.com>
2026-01-28 23:12:34 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     Настройки приложения, загружаемые из переменных окружения.
     """
     # Настройки базы данных
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://perfume_user:cER5DO6SVZxzLjoxOufttAJU5XTXK8ju@dpg-d5ifb156ubrc738j2vig-a.virginia-postgres.render.com/perfume_store_xqdm")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql")
 
     # Настройки JWT
     SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key-should-be-at-least-32-characters")
@@ -21,7 +21,7 @@ class Settings(BaseSettings):
 
     # Настройки CORS
     CORS_ORIGINS: list = [
-        "https://src-tjpz.onrender.com" # Продакшен URL (если есть)
+        "https://dediparfum.ru" # Продакшен URL (если есть)
     ]
 
     # Настройки логирования
